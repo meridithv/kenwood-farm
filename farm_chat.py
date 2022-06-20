e7646035d410e3465c7184b94b85e6ca1b5d2c6e
Meridith Viguet <meridith.viguet@gmail.com>
2022-06-20 15:51:56 -0500

    Create SQLite database for users and posts.

--- farm_chat.py
+++ farm_chat.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template, url_for
-#from forms import RegistrationForm, LoginForm
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__) 
+app.config.update(dict(
+    SECRET_KEY="20f8ac6ebf5a6dc2842b81235d95f034",
+))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-'''app.config['SECRET KEY'] = '20f8ac6ebf5a6dc2842b81235d95f034'
-'''
+class User(db.Model):
+	id = db.Column(db.Integer(), primary_key = True)
+	username = db.Column(db.String(40), unique = True, nullable = False)
+	email = db.Column(db.String(120), unique = True, nullable = False)
+	image_file = db.Column(db.String(20), nullable = False, default='default.jpg')
+	password = db.Column(db.String(60), nullable = False)
+	posts = db.relationship('Post', backref = "author", lazy = True)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+	id = db.Column(db.Integer(), primary_key = True)
+	title = db.Column(db.String(100), nullable = False)
+	date_posted = db.Column(db.DateTime(), nullable = False, default = datetime.utcnow)
+	content = db.Column(db.Text(), nullable = False)
+	user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable = False)
+
+	def __repr__(self):
+		return f"Post('{self.title}', '{self.date_posted}'"
 
 posts = [
 	{
@@ -19,7 +44,7 @@ posts = [
 		'content': 'She should have gone to class in 2012 lorem ipsum.',
 		'date_posted': 'June 15, 2022'
 	}
-]
+] 
 
 @app.route("/")
 @app.route("/discuss")
@@ -27,10 +52,19 @@ def discuss():
 	return render_template('discuss.html', posts=posts)
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register(): 
 	form = RegistrationForm() 
+	if form.validate_on_submit():
+		flash(f'Thanks, {form.first_name.data}! You\'ve created your account.', 'success')
+		return redirect(url_for('discuss'))
 	return render_template('register.html', title='Sign up', form=form)
+
+
+@app.route("/login")
+def login(): 
+	form = LoginForm() 
+	return render_template('login.html', title='Log in', form=form)
 
 
 if __name__ == '__main__':
