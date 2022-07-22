b31199006c5d7854a784b04ba01ecac7615934bf
Meridith Viguet <meridith.viguet@gmail.com>
2022-07-22 01:40:55 -0500

    Clean up UI and polish login and registration error messages.

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: ["./farm_chat/templates/*.{html,js}"],
+  theme: {
+    colors: {
+      forest: '#242D24',
+      ecru: '#F9F4EB',
+      anguilla: '#1A87A1',
+      tahiti: '#40B5C3',
+      pool: '#7EC8C1',
+      wine: '#6D000A',
+      mulberry: '#AD002B',
+      mauve: '#AD4141',
+      rust: '#C82813',
+      fever: '#E51219',
+      longhorn: '#D6491C',
+      clem: '#FA831F',
+      marigold: '#FA9D2C',
+      creamsoda: '#FFF5CB',
+    screens: {
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    fontFamily: {
+      plex: 'Plex, sans-serif',
+      libre: 'Libre, serif',
+      bitter: 'Bitter, serif',
+      bungee: 'Bungee, sans-serif',
+      yeseva: 'Yeseva, serif'
+    },
+    },
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+
+  plugins: [],
+}
